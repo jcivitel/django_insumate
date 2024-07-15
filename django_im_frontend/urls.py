a4c6b865d52c07023bc2e321bc2f9314a16f6041
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2024-07-15 11:04:27 +0200

    added basic html forms

--- django_im_frontend/urls.py
+++ django_im_frontend/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- django_im_frontend/views.py
+++ django_im_frontend/views.py
@@ -1,12 +1,33 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.template import loader
 
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, "login.html")
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
 @login_required
 def dashboard(request):
-    template = loader.get_template("projects/project_list.html")
+    template = loader.get_template("dashboard.html")
 
     template_opts = dict()
 
