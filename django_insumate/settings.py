982458052aa92f48c0a85bd63f3fb00c2607e962
Jan Civitelli Benso <jan@civitelli.de>
2024-07-17 15:54:46 +0200

    just allow all

--- django_insumate/settings.py
+++ django_insumate/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config("SECRET_KEY", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default=["*"], cast=Csv())
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
