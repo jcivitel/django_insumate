e57ee6eec2b5f17b392362feb2271952bfed369d
Jan Civitelli Benso <jan@civitelli.de>
2024-08-20 14:29:32 +0200

    implement more api functions

--- django_im_api/urls.py
+++ django_im_api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserExport, ProductInfoViewSet
+from .views import (
+    ProductInfoViewSet,
+    UserProfileExport,
+    MealEntryExport,
+    RecentSearchExport,
+)
 
 router = DefaultRouter()
+router.register(r"userinfo", UserProfileExport, basename="userinfo")
+router.register(r"meal-entries", MealEntryExport, basename="meal-entries")
+router.register(r"recent-searches", RecentSearchExport, basename="recent-searches")
 router.register(r"product", ProductInfoViewSet, basename="product")
-router.register(r"user", UserExport, basename="user")
-
 urlpatterns = [
     path("v1/", include(router.urls)),
 ]
